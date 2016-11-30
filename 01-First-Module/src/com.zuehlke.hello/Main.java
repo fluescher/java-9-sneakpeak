25c9d2236977037584298e868318859a3f21ce17
Florian Luescher <florian.luescher@zuehlke.com>
2016-11-30 12:23:57 +0100

    Add first module

---
+++ 01-First-Module/src/com.zuehlke.hello/Main.java
@@ -0,0 +1,9 @@
+package com.zuehlke.hello;
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("Greetings!");
+	}
+}
---
+++ 01-First-Module/src/com.zuehlke.hello/module-info.java
@@ -0,0 +1,1 @@
+module com.zuehlke.hello {}
