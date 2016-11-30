706fcbfdc58c2c8d439de3ca07d4aa878ef80070
Florian Luescher <florian.luescher@zuehlke.com>
2016-11-30 13:48:03 +0100

    Add new JMod packaging

---
+++ 03-JMod/src/com.zuehlke.tvm/Main.java
@@ -0,0 +1,11 @@
+package com.zuehlke.tvm;
+
+import com.zuehlke.tvm.hal.typea.Hardware;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println(new Hardware().name());		
+
+	}
+}
